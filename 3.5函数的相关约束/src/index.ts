aa518d2de0c7479de7f3e35ecbca0dfbf183ba37
mengxi <2515409089@qq.com>
2021-01-16 20:57:53 +0800

    3.5

--- 3.5函数的相关约束/src/index.ts
+++ 3.5函数的相关约束/src/index.ts
@@ -88,3 +88,43 @@ function getUsers(g: Gender): User[] {
 }
 
 getUsers("女");
+
+
+// 函数的相关约束
+
+/**
+ * 得到a*b的结果
+ * @param a 
+ * @param b 
+ */
+function combine(a: number, b: number): number;
+/**
+ * 得到a和b拼接的结果
+ * @param a 
+ * @param b 
+ */
+function combine(a: string, b: string): string;
+function combine(a: number | string, b: number | string): number | string {
+    if (typeof a === "number" && typeof b === "number") {
+        return a * b;
+    }
+    else if (typeof a === "string" && typeof b === "string") {
+        return a + b;
+    }
+    throw new Error("a和b必须是相同的类型");
+}
+
+const result = combine(3, 3);
+
+
+
+function sum(a: number, b: number, c?: number) {
+    if (c) {
+        return a + b + c;
+    } else {
+        a + b;
+    }
+}
+
+sum(3, 4);
+sum(3, 4, 5);
