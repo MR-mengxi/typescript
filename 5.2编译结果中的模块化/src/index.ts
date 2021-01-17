37fbdec1e72ee9170d1434bb655c868ed3338f21
mengxi <2515409089@qq.com>
2021-01-17 19:42:37 +0800

    5.3

--- 5.2编译结果中的模块化/src/index.ts
+++
@@ -1,8 +0,0 @@
-import sayHello, { name, sum } from "./myModule";
-
-
-console.log(name);
-
-console.log(sum(3, 4));
-
-sayHello();
---
+++ 5.3解决默认导入的错误/src/index.ts
@@ -0,0 +1,6 @@
+// import * as fs from "fs" // module.exports = {}
+// fs.readFileSync("./");
+
+// 在tsconfig.json中配置后可以使用默认导入
+import fs from "fs" // module.exports = {}
+fs.readFileSync("./");
